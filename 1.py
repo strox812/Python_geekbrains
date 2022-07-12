150119bb6bdbf0bd1972a875f2c68c60570d2792
Alexei Storozhev <65788722+strox812@users.noreply.github.com>
2022-07-12 09:26:18 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,7 +1,9 @@
 num_1 = int(input())
 num_2 = int(input())
-def divided (num_1, num_2):
+def devided (num_1, num_2):
     result = num_1 // num_2
-    print(result)
-
-divided(num_1,num_2)
+    if num_1 != 0:
+        print(result)
+    else:
+        print("На ноль делить нельзя")
+devided(num_1,num_2)
